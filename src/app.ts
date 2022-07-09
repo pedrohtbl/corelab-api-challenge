efa1a408b2d72b643cf15baa230591f9456c37ea
Phtbl <pedrohtbl@gmail.com>
2022-07-09 17:33:32 -0300

    fix: cors adicionado

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express"
 import sessionRouter from "./routes/session.route"
 import userRouter from "./routes/user.routes"
 import vehiclesRouter from "./routes/vehicles.routes"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/vehicles', vehiclesRouter)
 app.use('/users', userRouter)
